2f49aa72e72bed602c7dd38f82881b3d664df3fc
Walid Chtioui <walid.chtioui.main@gmail.com>
2025-07-15 13:39:58 +0200

    add python; add glsl analyzer; add bash lsp; add cmake; add protobuf;

--- lua/cgnvim/configs/mason-lspconfig.lua
+++ lua/cgnvim/configs/mason-lspconfig.lua
@@ -1,5 +1,13 @@
 local options = {
-  ensure_installed = { "clangd", "lua_ls" }
+  ensure_installed = {
+    "clangd",
+    "pyright",
+    "glsl_analyzer",
+    "lua_ls",
+    "bashls",
+    "cmake",
+    "protols",
+  }
 }
 
 return options

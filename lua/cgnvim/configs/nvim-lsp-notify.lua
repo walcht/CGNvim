71a3db21c5510506280018f3cf14801744eb8e8a
Walid Chtioui <walid.chtioui.main@gmail.com>
2025-07-27 13:27:56 +0200

    remove nvim lsp notify plugin;

--- lua/cgnvim/configs/nvim-lsp-notify.lua
+++
@@ -1,3 +0,0 @@
-return {
-  notify = require("notify")
-}
--- lua/cgnvim/plugins/nvim-lsp-notify.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "mrded/nvim-lsp-notify",
-  version = "*",
-  lazy = false,
-  dependencies = { 'nvim-tree/nvim-web-devicons', 'rcarriga/nvim-notify' },
-  opts = function()
-    return require("cgnvim.configs.nvim-lsp-notify")
-  end,
-}

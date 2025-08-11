a78bffd762e26acea3acc490a0d827c627e3da48
Walid Chtioui <walid.chtioui.main@gmail.com>
2025-08-11 10:45:13 +0200

    overwrite vim.notify with fidget.notify;

--- lua/cgnvim/configs/fidget.lua
+++ lua/cgnvim/configs/fidget.lua
@@ -1,1 +1,11 @@
-return {}
+return {
+  progress = {
+    display = {
+      done_ttl = 10,
+    },
+  },
+  -- Options related to notification subsystem
+  notification = {
+    override_vim_notify = true, -- Automatically override vim.notify() with Fidget
+  },
+}

9de6c8459fdeb2e7e2641848051af9f0bd228a60
Walid Chtioui <walid.chtioui@ensi-uma.tn>
2025-12-17 22:38:06 +0100

    feat: add markdown-preview

---
+++ lua/cgnvim/plugins/markdown-preview.lua
@@ -0,0 +1,12 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && npm install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+  opts = function()
+    return require("cgnvim.configs.markdown-preview")
+  end,
+}

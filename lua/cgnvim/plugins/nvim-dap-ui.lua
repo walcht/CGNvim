3ef783946a1d4ef8b25250eacaa7b642bd516b73
Walid Chtioui <walid.chtioui@ensi-uma.tn>
2026-03-24 12:55:53 +0100

    chore: remove nvim-dap-ui

--- lua/cgnvim/plugins/nvim-dap-ui.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "rcarriga/nvim-dap-ui",
-  dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-  event = { "VeryLazy" },
-  lazy = false,
-  opts = function()
-    return require("cgnvim.configs.nvim-dap-ui")
-  end,
-}
